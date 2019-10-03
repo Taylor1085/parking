20f511e79e458408fed193c6cc10e76efd9687ea
Taylor <tcour10@gmail.com>
2019-10-02 20:05:59 -0500

    Tickets index html modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'tickets/index'
-  get 'tickets/show'
+  get '/tickets', to: 'tickets#index'
+  get '/tickets/:id', to: 'tickets#show', id: /\d+/
+
+  root to: 'tickets#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
