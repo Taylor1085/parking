309143b46f38b28c162eea1137f292ddf69c5b1b
Taylor <tcour10@gmail.com>
2019-10-07 21:17:02 -0500

    Commit routes change

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,6 @@ Rails.application.routes.draw do
   resources :tickets, only: %i[index show]
   resources :drivers, only: %i[index show]
   resources :about, only: %i[index]
-  # get 'about/index'
-  # get '/tickets', to: 'tickets#index'
-  # get '/tickets/:id', to: 'tickets#show', id: /\d+/
-  # get '/drivers', to: 'drivers#index'
-  # get '/drivers/:id', to: 'drivers#show', id: /\d+/
-  # get '/drivers/:id/payments', to: 'payments#show', id: /\d+/
-  # get '/about', to: 'about#index'
 
   root to: 'tickets#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
